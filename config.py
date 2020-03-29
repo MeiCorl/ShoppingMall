1e61370f62020ffbf7f96f6cb1b56ce524dbd254
caomei <caomei@tencent.com>
2020-03-29 13:35:48 +0800

    修改redis通信方式

--- config.py
+++ config.py
@@ -36,8 +36,10 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_HOURS = 24
 
-# 商户后台订阅的redis主题
+# merchants_listener_topic用于通知商户后端接收新消息， merchants_message_queue作为商户后端消息队列
 merchants_listener_topic = "merchants_listener"
+merchants_message_queue = "merchants_message_queue"
 
-# 小程序后台订阅的主题
+# miniapp_listener_topic用于通知小程序后端接收新消息， miniapp_message_queue作为小程序后端消息队列
 miniapp_listener_topic = "miniapp_listener"
+miniapp_message_queue = "miniapp_message_queue"
